fd2f8c4f99593cadc33cfb19e9b7232976145414
rickyzatnika <rickyzatnika91@gmail.com>
2023-02-09 08:12:35 +0700

    build

--- next.config.js
+++ next.config.js
@@ -4,18 +4,18 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "images.pexels.com"],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/invitation/:uuid",
-  //       destination: "/invitation/[unique_Code]",
-  //     },
-  //   ];
-  // },
-  // webpack: (config) => {
-  //   config.resolve.fallback = { fs: false };
-  //   return config;
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/invitation/:uuid",
+        destination: "/invitation/[unique_Code]",
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
