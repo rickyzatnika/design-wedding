68bb51b73dafa4dd05e3ef0e7ee20fa0e746696b
rickyzatnika <rickyzatnika91@gmail.com>
2023-02-12 05:08:59 +0700

    add meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { useState, useEffect } from "react";
 import { Layout } from "../components";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   const [showChild, setShowChild] = useState(false);
@@ -17,7 +18,17 @@ export default function App({ Component, pageProps }) {
   } else {
     return (
       <>
-
+        <Head>
+          <title>Undangan Pernikahan Rahmadi & Dinda</title>
+          <meta name="description" content="Webhouse Invitation - Buat undangan pernikahanmu yang spesial dengan web undangan kami. Temukan desain dan pilihan yang sesuai dengan kebutuhanmu."></meta>
+          <meta name="keywords" content="Webhouse Invitation, web undangan pernikahan, undangan pernikahan online, desain undangan pernikahan, webhouse invitation"></meta>
+          <meta name="robots" content="index, follow"></meta>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:image" content="https://amade.vercel.app/img/image-7.JPG"></meta>
+          <meta property="og:title" content="Webhouse Invitation - Web Undangan Pernikahan Rahmadi & Dinda" />
+          <meta property="og:description" content="Webhouse Invitation - Buat undangan pernikahanmu yang spesial dengan web undangan kami. Temukan desain dan pilihan yang sesuai dengan kebutuhanmu." />
+          <meta property="og:url" content="https://amade.vercel.app" />
+        </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
