23a3a7441c23b708926046a0c8ca3aad70252797
rickyzatnika <rickyzatnika91@gmail.com>
2023-02-13 04:43:34 +0700

    test

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='w-full h-screen flex items-center justify-center bg-[#161616] text-zinc-300'>404 PAGE NOTE FOUND</div>
+    )
+}
+
+export default NotFound
--- pages/invitation/[uuid].js
+++ pages/invitation/[uuid].js
@@ -12,6 +12,9 @@ import {
 import axios from "axios";
 
 const GuestInvitation = ({ guest, posts }) => {
+
+  if (!guest) return <h1 className="w-full h-screen flex items-center justify-center bg-[#131313] text-zinc-200">Uppss ....</h1>
+
   return (
     <>
       <Header />
@@ -28,41 +31,29 @@ const GuestInvitation = ({ guest, posts }) => {
 
 export default GuestInvitation;
 
-// export const getStaticPaths = async () => {
-//   const res = await axios.get(`${process.env.NEXT_PUBLIC_PRO_URI}/invitation`);
-//   const invitationDataList = res.data || [];
-//   const paths = invitationDataList.length
-//     ? invitationDataList.map((guests) => {
-//       return {
-//         params: {
-//           uuid: guests.unique_Code,
-//         },
-//       };
-//     })
-//     : [];
-//   return {
-//     paths,
-//     fallback: true,
-//   };
-// };
+export const getStaticPaths = async () => {
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_PRO_URI}/invitation`);
+  const invitationDataList = res.data || [];
+  const paths = invitationDataList.length
+    ? invitationDataList.map((guests) => {
+      return {
+        params: {
+          uuid: guests.unique_Code,
+        },
+      };
+    })
+    : [];
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
-// export const getStaticProps = async ({ params }) => {
-//   const res = await axios.get(
-//     `${process.env.NEXT_PUBLIC_PRO_URI}/invitation/${params.uuid}`
-//   );
-//   const guest = res.data || {};
-//   return {
-//     props: {
-//       guest,
-//     },
-//     revalidate: 1
-//   };
-// };
-
-export const getServerSideProps = async ({ params }) => {
+export const getStaticProps = async ({ params }) => {
   const res = await axios.get(
     `${process.env.NEXT_PUBLIC_PRO_URI}/invitation/${params.uuid}`
   );
+
   const guest = res.data || {};
 
   const res2 = await axios.get(
@@ -73,8 +64,29 @@ export const getServerSideProps = async ({ params }) => {
   return {
     props: {
       guest,
-      posts,
-      revalidate: 1
+      posts
     },
+    revalidate: 1
   };
 };
+
+
+// export const getServerSideProps = async ({ params }) => {
+//   const res = await axios.get(
+//     `${process.env.NEXT_PUBLIC_PRO_URI}/invitation/${params.uuid}`
+//   );
+//   const guest = res.data || {};
+
+//   const res2 = await axios.get(
+//     `${process.env.NEXT_PUBLIC_DEV_GET}/api/comment`
+//   );
+//   const posts = res2.data || {};
+
+//   return {
+//     props: {
+//       guest,
+//       posts,
+//       revalidate: 1
+//     },
+//   };
+// };
